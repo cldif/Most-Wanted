31cd62849866bc53d76a40a0ce81b01b10246761
Sylvain BESSONNEAU <sybessonne@a106imac05.local>
2021-01-04 10:36:19 +0100

    add tp2

---
+++ TP2/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sylvain BESSONNEAU on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TP2/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sylvain BESSONNEAU on 04/01/2021.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
