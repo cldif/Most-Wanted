2839c3194bcc6008584feeca45bb714358e3dbca
Clement DIF <cldif@a106imac05.local>
2021-01-11 09:34:15 +0100

    :tada: Added template project.

---
+++ Most-Wanted/Most_WantedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Most_WantedApp.swift
+//  Most-Wanted
+//
+//  Created by Clement DIF on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Most_WantedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
